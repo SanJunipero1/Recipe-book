6c71a5d1359bb844df1d85fe03f1adcb721371d0
Clickadelic <toby.hopp@gmail.com>
2022-11-09 21:01:34 +0100

    Receipe Search, Breadcrumbs added

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -13,17 +13,19 @@ export default function Search ({setSearchValue}){
     }
 
     return(
-        <div class="wrap-slideshow">
+        <div className="App-receipe-search">
             <div className="container">
                 <div className="row">
-                    <div className="col text-center">
+                    <div className="offset-md-4 col-md-4">
                         <div className="input-group">
                             <div className="form-outline">
-                                <input onChange={handleChange} type="search" id="form1" className="form-control" />
-                                <label className="form-label" for="form1">Search</label>
+                                <input onChange={handleChange} type="search" id="form1" className="form-control" placeholder="Was essen wir heute?" />
+                                <label className="form-label visually-hidden" for="form1">Rezeptsuche</label>
                             </div>
-                            <button type="button" onClick={runSearch} className="btn btn-primary">
-                                <i className="fas fa-search"></i>
+                            <button type="button" onClick={runSearch} className="btn btn-primary text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                </svg>
                             </button>
                         </div>
                     </div>
